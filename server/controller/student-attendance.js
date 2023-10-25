77ad7cb4c305efae0572cdc6cf7ae8a918b81d80
RubelMehmed <rubel.49@geography-juniv.edu.bd>
2023-10-26 00:10:10 +0600

    feat: attendance system routes

--- server/controller/student-attendance.js
+++ server/controller/student-attendance.js
@@ -6,19 +6,37 @@ const StudentAttedance = require('../models/StudentAttendance');
 const error = require('../utils/error');
 
 const getAttendance = async (req, res) => {
+    const { id } = req.params;
 
     try {
         const adminAttendance = await AdminAttendance.findById(id);
         if (!adminAttendance) {
             throw error('Attendance not found', 404);
         }
+        if (adminAttendance.status === 'COMPLETED') {
+            throw error('Attendance is already completed', 400); 
+        }
+        
+        let attendance = await StudentAttendance.findOne({user: req.user._id, adminAttendance: id});
+        if (attendance) {
+            throw error('Attendance is already marked', 400);
+        }
+        
+        attendance = new StudentAttendance({
+            user: req.user._id,
+            adminAttendance: id,
+        })
+
+        await attendance.save();
+        return res.status(201).json(attendance);
+
     } catch (e) {
         next(e);
         
     }
 };
 
-const getStudentAttendance = async (req, res) => {
+const getStudentAttendance = async ( _req, res, next) => {
     try {
         const started = addMinutes( new Date(running.createdAt), running.timeLimit);
         if (isAfter(newDate(), started)) {
@@ -30,3 +48,29 @@ const getStudentAttendance = async (req, res) => {
         
     }
 };
+
+const getAttendanceStatus = async (_req, res, next) => {
+    try {
+        const running = await findOne({status: 'RUNNING'});
+        if (!running){
+            throw error('Not Running', 400)
+        }
+
+        const started = addMinutes( new Date(running.createdAt), running.timeLimit);
+        if (isAfter(newDate(), started)) {
+            running.status = 'COMPLETED';
+            await running.save();
+        }
+        return res.status(200).json(running)
+    } catch (e) {
+        next(e)
+    }
+    
+}
+
+
+module.exports = {
+    getAttendance,
+    getStudentAttendance,
+    getAttendanceStatus,
+}
